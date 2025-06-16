c3869fae766afc933fc3ea863d1fa93c39c70cbf
Arianna Smith-Forte <ariannasforte@meta.com>
2025-06-16 16:18:33 -0700

    feature/addition of header.jsx

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Header = ({searchTerm, setSearchTerm, handleSearch, handleClear, setFilter}) => {
+    const filterOptions = ['All', 'Recent', 'Celebration', 'Thank You', 'Inspiration'];
+    return (
+        <header className= "header-container">
+            <h1> Kudos Board</h1>
+            
+            <section className= "search-bar">
+                <input
+                    type= "text"
+                    placeholder='Search boards...'
+                    value = {searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
+                <button onClick={handleClear}>Clear</button>
+
+            </section>
+
+            <nav className= "filter-controls">
+                {filterOptions.map((option) => (
+                    <button key={option} onClick={() => setFilter(option)}>
+                        {option}
+                </button>
+                ))}
+            </nav>
+            
+            <button onClick={() => alert("TODO: Add Board logic")}>Add Board</button>
+        </header>
+    );
+};
+
+export default Header;
